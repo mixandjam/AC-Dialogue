fc4627861c81e9486ba0468f93ee4ffa12abdd55
André Cardoso <amc.acardoso@gmail.com>
2020-03-15 21:35:43 +0100

    jammo + animation rigging + cinemachine transition + dotween + ui tests

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Volume dialogueDof;
+
+    public GameObject gameCam;
+    public GameObject dialogueCam;
+
+    public CanvasGroup uiGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameCam.SetActive(!gameCam.active);
+            dialogueCam.SetActive(!dialogueCam.active);
+
+            float dofWeight = dialogueCam.active ? 1 : 0;
+            DOVirtual.Float(dialogueDof.weight, dofWeight, .8f, DialogueDOF);
+
+            uiGroup.DOFade(dofWeight, .2f).SetDelay(dialogueCam.active ? .65f : 0);
+            if (dofWeight == 1) uiGroup.transform.DOScale(.5f, .2f).From().SetEase(Ease.OutBack).SetDelay(.65f);
+        }
+    }
+
+    public void DialogueDOF(float x)
+    {
+        dialogueDof.weight = x;
+    }
+}
---
+++ Assets/Scripts/NPC/NpcAim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class NpcAim : MonoBehaviour
+{
+    public Transform aimController;
+    private Transform aimTarget;
+    public MultiAimConstraint multiAim;
+
+    void Update()
+    {
+        float weight = (aimTarget == null) ? 0 : 1;
+        Vector3 pos = (aimTarget == null) ? transform.position + transform.forward : aimTarget.position;
+
+        multiAim.weight = Mathf.Lerp(multiAim.weight, weight, .1f);
+        aimController.position = Vector3.Lerp(aimController.position, pos, .1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            aimTarget = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            aimTarget = null;
+    }
+
+}
