6dad494ccb3580649b2b5eca327a5423124cb082
William Enders <56558099+EndersWilliam@users.noreply.github.com>
2020-03-17 18:14:11 -0400

    moved text parsing and animating to TMP_Animated component

---
+++ Assets/TMP_Animated/Runtime/TMP_Animated.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TMPro
+{
+    public enum Emotion { happy, sad, suprised, angry };
+    [System.Serializable] public class EmotionEvent : UnityEvent<Emotion> { }
+    public class TMP_Animated : TextMeshProUGUI
+    {
+
+        [SerializeField] private float speed = 10;
+        public EmotionEvent onEmotionChange;
+        public void ReadText(string newText)
+        {
+            // split the whole text into parts based off the <> tags 
+            // even numbers in the array are text, odd numbers are tags
+            string[] subTexts = newText.Split('<', '>');
+
+            // textmeshpro still needs to parse its built-in tags, so we only include noncustom tags
+            string displayText = "";
+            for (int i = 0; i < subTexts.Length; i++)
+            {
+                if (i % 2 == 0)
+                    displayText += subTexts[i];
+                else if (!isCustomTag(subTexts[i].Replace(" ", "")))
+                    displayText += $"<{subTexts[i]}>";
+            }
+            // check to see if a tag is our own
+            bool isCustomTag(string tag)
+            {
+                return tag.StartsWith("speed=") || tag.StartsWith("pause=") || tag.StartsWith("emotion=");
+            }
+
+            // send that string to textmeshpro and hide all of it, then start reading
+            text = displayText;
+            maxVisibleCharacters = 0;
+            StartCoroutine(Read());
+
+            IEnumerator Read()
+            {
+                int subCounter = 0;
+                int visibleCounter = 0;
+                while (subCounter < subTexts.Length)
+                {
+                    // if 
+                    if (subCounter % 2 == 1)
+                    {
+                        yield return EvaluateTag(subTexts[subCounter].Replace(" ", ""));
+                    }
+                    else
+                    {
+                        while (visibleCounter < subTexts[subCounter].Length)
+                        {
+                            visibleCounter++;
+                            maxVisibleCharacters++;
+                            yield return new WaitForSeconds(1f / speed);
+                        }
+                        visibleCounter = 0;
+                    }
+                    subCounter++;
+                }
+                yield return null;
+
+                WaitForSeconds EvaluateTag(string tag)
+                {
+                    if (tag.Length > 0)
+                    {
+                        if (tag.StartsWith("speed="))
+                        {
+                            speed = float.Parse(tag.Split('=')[1]);
+                        }
+                        else if (tag.StartsWith("pause="))
+                        {
+                            return new WaitForSeconds(float.Parse(tag.Split('=')[1]));
+                        }
+                        else if (tag.StartsWith("emotion="))
+                        {
+                            onEmotionChange.Invoke((Emotion)System.Enum.Parse(typeof(Emotion), tag.Split('=')[1]));
+                        }
+                    }
+                    return null;
+                }
+            }
+        }
+    }
+}
--- Assets/TestingTextParser.cs
+++ Assets/TestingTextParser.cs
@@ -3,67 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TMP_Animated))]
 public class TestingTextParser : MonoBehaviour
 {
+
     [TextArea] public string masterText;
-    public TextMeshProUGUI gui;
-    // Start is called before the first frame update
+    private TMP_Animated animatedText;
+    private void Awake()
+    {
+        animatedText = GetComponent<TMP_Animated>();
+    }
     void Start()
     {
-        ReadText(masterText);
+        animatedText.ReadText(masterText);
+        animatedText.onEmotionChange.AddListener((newEmotion) => print(newEmotion));
     }
 
-    void ReadText(string text)
-    {
-        string[] subText = text.Split('<', '>');
-        string displayText = "";
-        for (int i = 0; i < subText.Length; i += 2)
-        {
-            displayText += subText[i];
-        }
-        int subCounter = 0;
-        int visibleCounter = 0;
-        float speed = 10;
-        gui.text = displayText;
-        gui.maxVisibleCharacters = 0;
-        StartCoroutine(Read());
-        IEnumerator Read()
-        {
-            while (subCounter < subText.Length)
-            {
-                if (subCounter % 2 == 1)
-                {
-                    EvaluateCommand();
-                }
-                else
-                {
-
-                    while (visibleCounter < subText[subCounter].Length)
-                    {
-                        visibleCounter++;
-                        gui.maxVisibleCharacters++;
-                        yield return new WaitForSeconds(1f / speed);
-                    }
-                    visibleCounter = 0;
-                }
-                subCounter++;
-            }
-            yield return null;
-        }
-        void EvaluateCommand()
-        {
-            string command = subText[subCounter].ToLower();
-            if (command.Length > 0)
-            {
-                if (command.StartsWith("speed"))
-                {
-                    speed = float.Parse(command.Split('=')[1]);
-                }
-                else if (command.StartsWith("emotion"))
-                {
-                    // set emotion
-                }
-            }
-        }
-    }
 }
