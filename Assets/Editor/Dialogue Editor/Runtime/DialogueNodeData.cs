2d990c1636a89dfbef19591eb000f43dc032bdf9
André Cardoso <amc.acardoso@gmail.com>
2020-03-19 01:44:06 +0100

    get out of here graph

--- Assets/Editor/Dialogue Editor/Runtime/DialogueNodeData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public class DialogueNodeData
-{
-    public string Guid;
-    public string DialogueText;
-    public Vector2 Position;
-}
--- Assets/Editor/Dialogue Editor/Runtime/NodeLinkData.cs
+++
@@ -1,7 +0,0 @@
-﻿[System.Serializable]
-public class NodeLinkData
-{
-    public string BaseNodeGuid;
-    public string PortName;
-    public string TargetNodeGuid;
-}
